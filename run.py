b0590152539bd76f5dbf0179a45bffc4f40a8dc9
OhimaiMatthew <matthew4ohimai15@gmail.com>
2024-12-20 11:23:15 +0100

    Run Application

---
+++ run.py
@@ -0,0 +1,11 @@
+from api import create_app
+import os
+from dotenv import load_dotenv
+
+"""Load all Environment variables"""
+load_dotenv()
+
+FLASK_ENV = os.getenv("FLASK_ENV") 
+"""Application Entry Point"""
+app = create_app(FLASK_ENV)
+
